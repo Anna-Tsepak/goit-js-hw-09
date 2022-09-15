288993ea8ecebf2fc267d3b4d409addfca26561d
Anna-Tsepak <108067730+Anna-Tsepak@users.noreply.github.com>
2022-09-15 17:29:44 +0300

    first finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnstart = document.querySelector('button[data-start]');
+const btnstop = document.querySelector('button[data-stop]');
+// const body = document.querySelector('body');
+btnstop.disabled = true;
+
+btnstart.addEventListener('click', onStart);
+
+let timerId;
+function onStart(event) {
+    btnstart.disabled = true; 
+    btnstop.disabled = false; 
+    timerId = setInterval(() => {
+        const bodyColor = getRandomHexColor();
+        document.body.style.background = bodyColor;
+    }, 1000);
+}
+
+btnstop.addEventListener('click', onStop);
+
+function onStop() {
+  btnstart.disabled = false;
+  btnstop.disabled = true;
+  clearInterval(timerId);
+}
+
+
 
