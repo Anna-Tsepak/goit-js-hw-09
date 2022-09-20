8a02f88aeb69d570627929e03a53e067f402b906
Anna-Tsepak <108067730+Anna-Tsepak@users.noreply.github.com>
2022-09-20 19:57:48 +0300

    finish second

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,86 +1,88 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
+ 
+import Notiflix from 'notiflix';
+ 
+const text = document.querySelector('#datetime-picker');
+const timerHtml = document.querySelector('.timer');
 
-const inputRef = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
-const timerDaysRef = document.querySelector('span[data-days]');
-const timerHoursRef = document.querySelector('span[data-hours]');
-const timerMinutesRef = document.querySelector('span[data-minutes]');
 const timerSecondsRef = document.querySelector('span[data-seconds]');
-console.dir(btnStart)
-let selectedTimeMs = null;
-
-let timerInterval;
+const timerMinutesRef  = document.querySelector('span[data-minutes]');
+const timerHoursRef  = document.querySelector('span[data-hours]');
+const timerDaysRef = document.querySelector('span[data-days]');
 
 btnStart.disabled = true;
-
-
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
-    minuteIncrement: 1,
-    onOpen() {
-      timerInterval && clearInterval(timerInterval)
-  },
-    
+  minuteIncrement: 1,
   onClose(selectedDates) {
-      if (selectedDates[0] < new Date()){
-          alert('Please choose a date in the future');
-      }
-      else {
-          btnStart.disabled = false;
-          selectedTimeMs = selectedDates[0].getTime();
-          console.log(selectedTimeMs)
-      }
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStart.disabled = true;
+    } else {
+      btnStart.disabled = false;
+    }
   },
 };
-let selectedDay = flatpickr(inputRef, options);
-btnStart.addEventListener('click', onBtnStartClick);
-
-function onBtnStartClick(event) {
-    btnStart.disabled = true;
-
-    let deltaTime = selectedTimeMs - Date.now();
-    console.log(deltaTime);
-    
-   
-    
-    timerInterval = setInterval(() => {
-
- const { days, hours, minutes, seconds } = convertMs(deltaTime);
-    
-    timerDaysRef.textContent = addZero(days);
-    timerHoursRef.textContent = addZero(hours);
-    timerMinutesRef.textContent = addZero(minutes);
-    timerSecondsRef.textContent = addZero(seconds);
-        
-        deltaTime -= 1000;
-    },1000)
-}
-
-function addZero(value)
-{
-    return String(value).padStart(2, '0');
-}
-
-
+flatpickr(text, options);
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+function onChangeContent({ days, hours, minutes, seconds }) {
+        timerDaysRef.textContent = days;
+        timerHoursRef.textContent = hours;
+        timerMinutesRef.textContent = minutes;
+        timerSecondsRef.textContent = seconds;
+}
+btnStart.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(text.value) - new Date();
+    btnStart.disabled = true;
+    if (countdown >= 0) {
+      const data = convertMs(countdown);
+      onChangeContent(data)
+      console.log(onChangeContent)
+      //  days.textContent = addLeadingZero(timeObject.days);
+      //  hours.textContent = addLeadingZero(timeObject.hours);
+      //  minutes.textContent = addLeadingZero(timeObject.minutes);
+      //  seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        timerHtml.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      timerHtml.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
